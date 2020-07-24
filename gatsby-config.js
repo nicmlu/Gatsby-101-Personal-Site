7c94eb50a6ff676d2712fd7419b1938ecf81fce9
nicmlu <NicoleMLucien@gmail.com>
2020-07-24 19:14:31 -0400

    update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,21 @@
 module.exports = {
+    siteMetadata: {
+        title: `Nicole Lucien`,
+        author: `Nicole Lucien`,
+        description: `Nicole Lucien's personal site for Gatsby 101 workshop at CodeLand 2020`,
+        siteUrl: `https://gatsby-101-personal-site-nic.netlify.app`,
+        alternateSite: `https://www.linkedin.com/in/nicolemlucien/`,
+        social: [
+            {
+                name: `Twitter`,
+                url: `https://twitter.com/HerNameisNic`,
+            },
+            {
+                name: `GitHub`,
+                url: `https://github.com/nicmlu`,
+            },
+        ],
+    },
     plugins: [
         `gatsby-plugin-mdx`, 
         {
