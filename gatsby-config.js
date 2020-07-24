0635a6861ba50afb3baa61aaa6839e84b079b9f3
nicmlu <NicoleMLucien@gmail.com>
2020-07-24 18:25:42 -0400

    Add Blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
 module.exports = {
     plugins: [
-        `gatsby-plugin-mdx`
+        `gatsby-plugin-mdx`, 
+        {
+            resolve: `gatsby-theme-blog`, 
+            options: {
+                basePath: `/blog`,
+                mdxOtherwiseConfigured: true
+            }
+        }
     ]
 }
