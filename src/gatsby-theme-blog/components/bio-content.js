c0e6379ce066fa1b0a57d36aa19056e452696178
nicmlu <NicoleMLucien@gmail.com>
2020-07-24 18:49:51 -0400

    update bio

---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Shadow me to add your own bio content
+ */
+
+const BioContent = () => (
+    <Fragment>
+        Words by <Styled.a href="http://example.com/">Nicole Lucien</Styled.a>.
+    <br />
+        Live. Laugh. Learn.
+  </Fragment>
+)
+
+export default BioContent
